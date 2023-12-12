45b23537b16bcf050adc215627b63246556e60f3
nikhil-namburi <64106047+nikhil-namburi@users.noreply.github.com>
2023-12-11 23:05:38 -0500

    Create import.py

---
+++ backend/import.py
@@ -0,0 +1,52 @@
+import json
+import sqlite3
+
+def get_english_name(properties):
+    return properties.get('name:en') or properties.get('name')
+
+def get_id(feature):
+    return feature.get('id')
+
+def read_geojson(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:  # Specify UTF-8 encoding here
+        return json.load(file)
+
+def create_database(db_name):
+    conn = sqlite3.connect(db_name)
+    conn.execute('''CREATE TABLE IF NOT EXISTS railroads (
+                    id TEXT, name TEXT, latitude REAL, longitude REAL, 
+                    line_data TEXT, current_pop INTEGER, max_capacity INTEGER)''')
+    return conn
+
+def insert_railroad_names(conn, names):
+    cursor = conn.cursor()
+    for entry in names:
+        # Insert data based on feature type
+        cursor.execute('''INSERT INTO railroads (id, name, latitude, longitude, 
+                       line_data, current_pop, max_capacity) VALUES 
+                       (?, ?, ?, ?, ?, NULL, NULL)''', entry)
+    conn.commit()
+
+def main():
+    geojson_data = read_geojson(r'Dyanmo\backend\rail_network.geojson')
+    db_conn = create_database('railroads.db')
+
+    railroad_names = []
+    for feature in geojson_data['features']:
+        id_value = get_id(feature)
+        name_value = feature['properties'].get('name:en') or feature['properties'].get('name')
+        print(id_value)
+
+        if feature['geometry']['type'] == 'Point':
+            latit, longit = feature['geometry']['coordinates']
+            railroad_names.append((id_value, name_value, latit, longit, None))
+        elif feature['geometry']['type'] == 'LineString':
+            # Convert line coordinates to a string or a suitable format
+            line_data = str(feature['geometry']['coordinates'])
+            railroad_names.append((id_value, name_value, None, None, line_data))
+
+    insert_railroad_names(db_conn, railroad_names)
+    db_conn.close()
+
+if __name__ == "__main__":
+    main()
